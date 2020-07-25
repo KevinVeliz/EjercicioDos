3436bc7ba0423ffdfd8d35c4667cc3856ace738d
Kevin Veliz <mario-veliz258934@hotmail.com>
2020-07-25 04:22:04 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+  
+double division (double ,double);  
+
+
+int main()
+{
+   int a,b; 
+   cout << "Introduce dividendo: ";
+   cin >> a;
+    cout << "Introduce divisor: ";
+   cin >> b;
+
+    if(b!=0)
+     cout <<a<< " / "<<b<<" = "<< division(a,b) << endl;
+ else
+    cout << "Divisor no puede ser cero" << endl;
+   return 0;
+  
+}
+
+//Funcion recursiva
+double division (double x,double y)
+{
+  double res;
+  if (x==y) 
+  return 1;
+  
+  if (x<y) 
+  return 0;
+  
+  if (x==0)
+  return 0;
+
+  if(x>y)
+  {
+    res = (division(x-y, y)+1);
+  }
+  return res ;
+}
+ 
